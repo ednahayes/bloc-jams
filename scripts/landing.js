1b4b9b1eec207dc3f7a223b98cf5b301112470e8
Edna Hayes <princessahe@hotmail.com>
2017-04-24 16:14:18 -0500

    assignment-9: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,4 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
 
     var points = document.getElementsByClassName('point');
 
@@ -8,8 +10,27 @@ var animatePoints = function() {
         points[index].style.msTransform = "scaleX(1) translateY(0)";
         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
-    for (var i = 0; i < points.length; i++) {
-      revealPoint(i);
+    function forEach(points) {
+      for (var i = 0; i < points.length; i++)
+      forEach(i);
     }
 
+
 };
+
+window.onload = function() {
+
+    if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+    }
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+
+    window.addEventLister('scroll', function(event) {
+         console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+              animatePoints(pointsArray);
+          }
+    });
+}
